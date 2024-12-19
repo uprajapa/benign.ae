86592b6b02d0317b9698223c0e0512a6c5746f72
Urvish Prajapati <urvishprajapati412@gmail.com>
2024-12-18 23:28:40 -0500

    Update Server connection

--- src/app/users/page.tsx
+++
@@ -1,8 +0,0 @@
-import { createClient } from "../../../utils/supabase/server";
-
-export default async function Users() {
-  const supabase = await createClient();
-  const { data : users }  = await supabase.from("users").select();
-  console.log(users);
-  return <>{JSON.stringify(users)}</>
-}
