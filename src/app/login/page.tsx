2e952ca731206f913f22080972878f3753121850
Urvish Prajapati <urvishprajapati412@gmail.com>
2024-12-20 22:36:02 -0500

    Remove Google auth

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,27 +1,5 @@
 export default function Login() {
   return (
       
-      <html>
-  <body>
-    <script src="https://accounts.google.com/gsi/client" async></script>
-    <script>
-      function handleCredentialResponse(response) {
-        console.log("Encoded JWT ID token: " + response.credential);
-      }
-      window.onload = function () {
-        google.accounts.id.initialize({
-          client_id: "Google_client_ID"
-          callback: handleCredentialResponse
-        });
-        google.accounts.id.renderButton(
-          document.getElementById("buttonDiv"),
-          { theme: "outline", size: "large" }  // customization attributes
-        );
-        google.accounts.id.prompt(); // also display the One Tap dialog
-      }
-    </script>
-    <div id="buttonDiv"></div>
-  </body>
-</html>
   );
 }
