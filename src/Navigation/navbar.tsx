d05fe00d691e67b34e9719bb2807cdb08be95ecf
Urvish Prajapati <urvishprajapati412@gmail.com>
2024-12-30 23:55:11 -0500

    Add navbar

---
+++ src/Navigation/navbar.tsx
@@ -0,0 +1,15 @@
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="logo"></div>
+      <div className="menu-bar">
+        <ul>
+          <li>Home page</li>
+          <li>Contact us</li>
+          <li>About us</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,8 @@
+import Navbar from "@/Navigation/navbar";
 export default function Login() {
   return (
-      
+      <>
+        <Navbar />
+      </>
   );
 }
