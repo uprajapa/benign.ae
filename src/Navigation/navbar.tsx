f270cd4567c0876bc5b361ff32cc0056270a673a
Urvish Prajapati <urvishprajapati412@gmail.com>
2025-01-02 23:27:57 -0500

    Add css

--- src/Navigation/navbar.tsx
+++ src/Navigation/navbar.tsx
@@ -1,11 +1,13 @@
 
 export default function Navbar() {
   return (
-    <div className="navbar">
-      <div className="logo"></div>
-      <div className="menu-bar">
-        <ul>
-          <li>Home page</li>
+    <div className="navbar w-auto mx-auto">
+      <div className="logo w-32">
+        <img src="./pdf.png" alt="" className="md:shrink-0"></img>
+      </div>
+      <div className="menu-bar md:flex-row">
+        <ul className="">
+          <li className="">Home page</li>
           <li>Contact us</li>
           <li>About us</li>
         </ul>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
       <script src="https://cdn.tailwindcss.com"></script>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       </head>
       <body>{children}</body>
     </html>
