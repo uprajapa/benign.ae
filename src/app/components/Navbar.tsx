52ae9a95bd939bd704d0db337d23ea20510bab89
Urvish Prajapati <urvishprajapati412@gmail.com>
2025-01-09 12:18:39 -0500

    Update Navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,13 +1,17 @@
+'use client';
 import Link from "next/link";
+import { useState } from "react";
+import burger from '../../../public/images/burger-menu.png';
 
 export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav className="bg-gray-500 text-white text- p-5 sm:p-4 md:flex md:justify-between md:items-center">
+    <nav className="bg-gray-500 text-white text- p-5 sm:p-3 md:flex md:justify-between md:items-center">
       <div className="container mx-auto flex justify-between items-center">
         <a href="" className="text-2xl font-bold">
           Benign
         </a>
-        <div>
+        <div className="hidden md:flex">
         <Link href="../" className="mx-2 hover:text-gray-700">
           Home 
         </Link>
@@ -17,6 +21,17 @@ export const Navbar = () => {
         <Link href="/store " className="mx-2 hover:text-gray-700">
           Store
         </Link>
+        </div>
+        <div className="md:hidden flex items-center">
+          <button onClick={() => {
+            setIsOpen(!isOpen)
+          }}>
+            <svg width="4rem" height="4rem" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g id="SVGRepo_bgCarrier" strokeWidth="0"/>
+              <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"/>
+              <g id="SVGRepo_iconCarrier"> <path d="M5 8H13.75M5 12H19M10.25 16L19 16" stroke="#ffffff" strokeLinecap="round" strokeLinejoin="round"/></g>
+            </svg>
+          </button>
         </div>
       </div>
     </nav>
